c7ad91f22f508348c5e1078fca46ce16813cf585
mikeliu89 <mikeliu89@hotmail.com>
2014-02-07 23:14:17 -0800

    Initial setup and commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FreshTomato::Application.config.secret_key_base = 'b3eda03d865093ded63b90abeb2191c25e0a51cde270d6e05cd35c7d003a45f0fae07e94631a0b4adcea9cd91b06f4b55eb5736b23e8567acbc1fd230804d8ca'
