67ba805e1212005d8b680360e83fc999f04adad0
Elison Deungria <edeungria@gmail.com>
2014-02-27 22:57:40 -0400

    Added create

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -9,11 +9,22 @@ class MoviesController < ApplicationController
 		@movie = Movie.find(id)
 	end
 
+	def create
+		@movie = Movie.create!(movie_params)
+		flash[:notice] = "'#{@movie.title}' created."
+		redirect_to movies_path
+	end
+
 	def destroy
 		id = params[:id]
 		@movie = Movie.find(id)
 		@movie.destroy
 		flash[:notice] = "'#{@movie.title}' deleted."
 		redirect_to movies_path
+	end
+
+	private
+	def movie_params
+		params.require(:movie).permit(:title, :rating, :description, :release_date)
 	end
 end
