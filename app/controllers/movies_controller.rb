2ae7e70b2be14725c05b9db8adcc11a6a5e2be9b
sleepingkirby <sleepingkirby@gmail.com>
2014-02-27 19:28:50 -0800

    What we did in class on 02-27-2014

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -8,10 +8,42 @@ class MoviesController < ApplicationController
 		@movie = Movie.find(params[:id])
 		logger.debug("MoviesController Show =================== #{params.inspect}")
 	end
+	def new
+                logger.debug("MoviesController new =================== #{params.inspect}")	
+	end
+
+
 	def destroy
 		@movie = Movie.destroy(params[:id])
 		logger.debug("MoviesController Destroy =================== #{params.inspect}")
 		flash[:notice] = "#{@movie.title} was deleted!"
 		redirect_to movies_path
 	end
+
+	def create
+		#debugger
+		logger.debug("MoviesController create movie...........>#{params.inspect}")
+ 		@movie = Movie.create!(movie_params)
+		flash[:notice] = "Movie \"#{@movie.title}\" was successfully created."
+		#instead of create a create view, we'd like to go to the movie list page after created a movie
+		redirect_to movies_path
+	end
+
+	def edit
+                @movie = Movie.find(params[:id])
+	end
+	def update
+                #debugger
+                logger.debug("MoviesController create movie...........>#{params.inspect}")
+		@movie = Movie.find params[:id]
+                @movie.update_attributes!(movie_params)
+                flash[:notice] = "Movie \"#{@movie.title}\" was successfully created."
+                #instead of create a create view, we'd like to go to the movie list page after created a movie
+                redirect_to movies_path
+	end 
+	private
+	def movie_params
+		params.require(:movie).permit(:title, :rating, :description, :release_date)
+	end
+
 end
