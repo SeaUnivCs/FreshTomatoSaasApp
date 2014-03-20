58072dba480711f02874a95aaa8d709f74e52b31
colca <colca7@gmail.com>
2014-03-19 18:36:20 -0700

    rm erb layout file

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 FreshTomato::Application.routes.draw do
+  get "static/index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
