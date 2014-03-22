59475b1697bb38970bfac276c601061ee2c061f0
Patrick Weisensee <pweisensee@gmail.com>
2014-03-21 22:10:35 -0700

    added defs for index

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -3,16 +3,59 @@ class MoviesController < ApplicationController
 	def index
 		@movies = Movie.all
 	end
+	
+	  # Show the new movie form
+	  def new
+	    @movie = Movie.new
+	  end
+	  
+	  # Create a new movie with given info from 'new' form and
+	  # redirect to movies page on success.
+	  def create
+	    @movie = Movie.new(movie_params)
+	    if @movie.save
+	      flash[:notice] = "#{@movie.title} was successfully created."
+	      redirect_to movies_path
+	    else
+	      # Create failed, re-render the form
+	      render 'new'
+	    end
+	  end
 
-	def show
-		id = params[:id]
-		@movie = Movie.find(id)
-	end
+	  # Show a single movie page
+	  def show
+	    id = params[:id]
+	    @movie = Movie.find(id)
+	  end
+	  
+	  # Show the edit form for a movie
+	  def edit
+	    @movie = Movie.find params[:id]
+	  end
 
-	def destroy
-		@movie = Movie.find(params[:id])
-		@movie.destroy 
-		flash[:notice] = "Movie '#{@movie.title}' deleted."
-		redirect_to movies_path
+	  # Update a movie with given info from 'edit' form and redirect
+	  # to the movie's page.
+	  def update
+	    @movie = Movie.find params[:id]
+	    if @movie.update_attributes(movie_params)
+	      flash[:notice] = "#{@movie.title} was successfully updated."
+	      redirect_to movie_path(@movie)
+	    else
+	      render 'edit'
+	    end
+	  end
+
+	  # Delete a movie and redirect to the movies page.
+	  def destroy
+	    @movie = Movie.find params[:id]
+	    @movie.destroy
+	    flash[:notice] = "Deleted '#{@movie.title}'."
+	    redirect_to movies_path
+	  end
+
+	  # Define movie parameters for validation
+	  private
+	  def movie_params
+	    params.require(:movie).permit(:title, :rating, :release_date, :description)
+	  end
 	end
-end
