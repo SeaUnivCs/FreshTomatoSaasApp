84f625beaf20866e438a528ec826c6d88da78bbf
colca <colca7@gmail.com>
2014-03-26 14:03:58 -0700

    add paperclip interpolates

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -2,7 +2,8 @@ class Movie < ActiveRecord::Base
   validates :title, :presence => true, :uniqueness => true, :length => {minimum: 3, too_short: "must have at least %{count} chars"}
   before_save :capitalize_title
   has_attached_file :pic, :styles => 
-	{ :medium => "300*300>", :thumb => "100*100>" }
+	{ :medium => "300*300>", :thumb => "100*100>" },
+        :url => ":s3_url"
 #        :storage => :s3, :s3_credentials => "#{Rails.root}/config/aws.yml",
 #        :path => ":attachment/:id/:style.:extension",
 #        :bucket => 'freshtomato'
--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -1,1 +1,4 @@
 Paperclip::Attachment.default_options[:s3_host_name] = 's3.amazonaws.com'
+Paperclip.interpolates(:s3_url) { |attachment, style|
+  "#{attachment.s3_protocol}://s3-us-west-2.amazonaws.com/#{attachment.bucket_name}/#{attachment.path(style).gsub(%r{^/},"")}"
+}
