179a7460814e1b98e66f296f92f9874290b0712e
colca <colca7@gmail.com>
2014-03-26 13:41:18 -0700

    delet s3 direct upload initializer

--- config/initializers/s3_direct_upload.rb
+++
@@ -1,7 +0,0 @@
-S3DirectUpload.config do |c|
-  c.access_key_id = "AKIAJWFYDWFXWXKZSCIA"
-  c.secret_access_key = "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"
-  c.bucket = "freshtomato"
-  c.region = "s3"
-  c.url = "https://#{c.bucket}.s3.amazonaws.com/"
-end
